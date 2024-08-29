58cea654ec19ca38897e6297f55b286aef0b3e69
Luis Giraldo <luisggon@protonmail.com>
2024-08-29 12:12:37 +0200

    fix bug

--- luxgiant_dstream/gwas.py
+++ luxgiant_dstream/gwas.py
@@ -95,7 +95,7 @@ class GWAS:
         plink_cmd1 = f"plink --bfile {os.path.join(results_dir, output_name)} --chr 1-22 --maf {maf} --geno {geno}  --hwe {hwe} --exclude {high_ld_regions_file} --range --indep-pairwise {ind_pair[0]} {ind_pair[1]} {ind_pair[2]} --threads {max_threads} --make-bed --out {os.path.join(results_dir, output_name+'_prunning')}"
 
         # LD pruning
-        plink_cmd2 = f"--bfile {os.path.join(results_dir, output_name+'_prunning')} --extract {os.path.join(results_dir, output_name+'_prunning.prune.in')} --make-bed --out {os.path.join(results_dir, output_name+'_LDpruned')} --threads {max_threads}"
+        plink_cmd2 = f"plink2 --bfile {os.path.join(results_dir, output_name+'_prunning')} --extract {os.path.join(results_dir, output_name+'_prunning.prune.in')} --make-bed --out {os.path.join(results_dir, output_name+'_LDpruned')} --threads {max_threads}"
 
         cmds = [plink_cmd1, plink_cmd2]
         for cmd in cmds:
