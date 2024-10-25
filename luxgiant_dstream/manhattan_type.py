c259fd77da7f79a7c404dbf639ef5f8744bf106d
Luis Giraldo <luisggon@protonmail.com>
2024-10-25 11:37:41 +0200

    change colors

--- luxgiant_dstream/manhattan_type.py
+++ luxgiant_dstream/manhattan_type.py
@@ -123,7 +123,7 @@ def manhattan_process_data(data_df:pd.DataFrame, chr_col:str='CHR', pos_col:str=
 
 def manhattan_draw(data_df:pd.DataFrame, snp_col:str, chr_col:str, pos_col:str, p_col:str, plot_dir:str, to_highlight:list=None, to_annotate:list=None, build:str='38', gtf_path:str=None)->bool:
 
-    chr_colors           = ['grey', 'skyblue']
+    chr_colors           = ['#66c2a5', '#fc8d62']
     ylab                 = "-log10(p)"
     xlab                 = "Chromosome"
     genome_line          = 5e-8
