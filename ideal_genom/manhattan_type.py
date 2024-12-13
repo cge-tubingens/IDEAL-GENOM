3072aaffdb0247981586b765f3605d2b7eec5bc4
Luis Giraldo <luisggon@protonmail.com>
2024-12-13 20:06:14 +0100

    change parameter

--- ideal_genom/manhattan_type.py
+++ ideal_genom/manhattan_type.py
@@ -170,7 +170,7 @@ def manhattan_process_data(data_df:pd.DataFrame, chr_col:str='CHR', pos_col:str=
 
     return manhattan_data
 
-def manhattan_draw(data_df:pd.DataFrame, snp_col:str, chr_col:str, pos_col:str, p_col:str, plot_dir:str, to_highlight:list=[], to_annotate:list=[], build:str='38', gtf_path:str=None, save_name:str='manhattan_plot.jpeg')->bool:
+def manhattan_draw(data_df:pd.DataFrame, snp_col:str, chr_col:str, pos_col:str, p_col:str, plot_dir:str, to_highlight:list=[], to_annotate:pd.DataFrame=None, gen_col:str=None, build:str='38', gtf_path:str=None, save_name:str='manhattan_plot.jpeg')->bool:
 
     """
     Draws a Manhattan plot for visualizing GWAS results.
@@ -220,8 +220,8 @@ def manhattan_draw(data_df:pd.DataFrame, snp_col:str, chr_col:str, pos_col:str, 
     for val in to_highlight:
         if not isinstance(val, str):
             raise TypeError("to_highlight must be a list of SNP identifiers.")
-    if not isinstance(to_annotate, list):
-        raise TypeError("to_annotate must be a list of SNP identifiers.")
+    if not isinstance(to_annotate, pd.DataFrame):
+        raise TypeError("to_annotate must be a data frame of SNP identifiers.")
     for val in to_annotate:
         if not isinstance(val, str):
             raise TypeError("to_annotate must be a list of SNP identifiers.")
