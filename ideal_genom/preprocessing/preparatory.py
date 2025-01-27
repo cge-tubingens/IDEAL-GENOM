f04614a04e9584e5506bab4a60926cbd4d39d908
Luis Giraldo <luisggon@protonmail.com>
2025-01-27 13:55:13 +0100

    corrections

--- ideal_genom/preprocessing/preparatory.py
+++ ideal_genom/preprocessing/preparatory.py
@@ -146,10 +146,10 @@ class Preparatory:
             memory = round(2*available_memory_mb/3,0)
 
         # plink command to exclude high LD regions
-        plink_cmd1 = f"plink --bfile {os.path.join(input_path, input_name)} --chr 1-22 --maf {maf} --geno {geno}  --hwe {hwe} --exclude {high_ld_regions_file} --range --indep-pairwise {ind_pair[0]} {ind_pair[1]} {ind_pair[2]} --threads {max_threads} --make-bed --out {os.path.join(results_dir, output_name+'_prunning')}"
+        plink_cmd1 = f"plink --bfile {os.path.join(input_path, input_name)} --chr 1-22 --maf {maf} --geno {geno}  --hwe {hwe} --exclude {high_ld_regions_file} --indep-pairwise {ind_pair[0]} {ind_pair[1]} {ind_pair[2]} --threads {max_threads} --memory {memory} --make-bed --out {os.path.join(results_dir, output_name+'_prunning')}"
 
         # plink command to perform LD pruning
-        plink_cmd2 = f"plink2 --bfile {os.path.join(results_dir, output_name+'_prunning')} --extract {os.path.join(results_dir, output_name+'_prunning.prune.in')} --make-bed --out {os.path.join(results_dir, output_name)} --threads {max_threads}"
+        plink_cmd2 = f"plink --bfile {os.path.join(results_dir, output_name+'_prunning')} --extract {os.path.join(results_dir, output_name+'_prunning.prune.in')} --make-bed --out {os.path.join(results_dir, output_name)} --threads {max_threads}"
 
         # execute plink commands
         cmds = [plink_cmd1, plink_cmd2]
