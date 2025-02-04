82d364240814bb99ce7fedeb38cd9e4b6f212fa1
Luis Giraldo <luisggon@protonmail.com>
2025-02-04 10:10:22 +0100

    new strcture

--- ideal_genom/__main__.py
+++ ideal_genom/__main__.py
@@ -10,7 +10,13 @@ from ideal_genom.gwas.gwas_glmm import GWASrandom
 
 def analysis_pipe(params_dict:dict, data_dict:dict, steps_dict:dict)->None:
 
-    if steps_dict['post_imp']:
+    post_imputation_params = params_dict['post_imputation']
+    preparatory_params     = params_dict['preparatory']
+    gwas_glm_params        = params_dict['gwas_glm']
+    gwas_glmm_params       = params_dict['gwas_glmm']
+
+    if steps_dict['pos_imputation']:
+
         # instantiate the PostImputation class
         post_imp = PostImputation(
             input_path = data_dict['input_directory'],
@@ -21,23 +27,27 @@ def analysis_pipe(params_dict:dict, data_dict:dict, steps_dict:dict)->None:
 
         # pipeline steps
         post_imp_steps = {
-            'unzip_chrom' : (post_imp.execute_unzip_chromosome_files, ()),
-            'filter_by_R2': (post_imp.execute_filter_variants, ()),
-            'normalize'   : (post_imp.execute_normalize_vcf, ()),
-            'annotate'    : (post_imp.execute_annotate_vcf, ()),
-            'index'       : (post_imp.execute_index_vcf, ()),
-            'concatenate' : (post_imp.execute_concat_vcf, ()),
-            'get_plink'   : (post_imp.get_plink_files, ()),
+            'unzip_chrom'  : (post_imp.execute_unzip_chromosome_files, {'password': post_imputation_params['zip_password']}),
+            'filter_by_R2' : (post_imp.execute_filter_variants, {'r2_threshold': post_imputation_params['r2_threshold']}),
+            'normalize'    : (post_imp.execute_normalize_vcf, {}),
+            'normalize_ref': (post_imp.execute_normalize_with_reference, {'reference_genome': post_imputation_params['ref_vcf']}),
+            'index'        : (post_imp.execute_index_vcf, {}),
+            'annotate'     : (post_imp.execute_annotate_vcf, {'annotations_file': post_imputation_params['ref_annotation']}),
+            'concatenate'  : (post_imp.execute_concat_vcf, {}),
+            'get_plink'    : (post_imp.get_plink_files, {}),
+            'clean_up'     : (post_imp.cleanup, {})
         }
 
         step_description = {
             'unzip_chrom' : 'Unzip chromosome files',
             'filter_by_R2': 'Filter imputed variants by R2',
             'normalize'   : 'Normalize VCF files',
-            'annotate'    : 'Annotate VCF files',
+            'normalize_ref': 'Normalize VCF files with reference genome',
             'index'       : 'Index VCF files',
+            'annotate'    : 'Annotate VCF files',
             'concatenate' : 'Concatenate VCF files',
-            'get_plink'   :  'Get PLINK files',
+            'get_plink'   : 'Get PLINK files',
+            'clean_up'    : 'Remove intermediate files'
         }
 
         for name, (func, params) in post_imp_steps.items():
@@ -46,73 +56,100 @@ def analysis_pipe(params_dict:dict, data_dict:dict, steps_dict:dict)->None:
 
         print("\033[1mPost-imputation steps completed.\033[0m")
 
-    if steps_dict['prep_ds']:
-        # instantiate the PrepDS class
-        prep = Preparatory(
-            input_path      =data_dict['input_directory'],
-            input_name      =data_dict['input_prefix'],
-            output_path     =data_dict['output_directory'],
-            output_name     =data_dict['output_prefix'],
-            dependables_path=data_dict['dependables_directory'],
+    if steps_dict['preparatory']:
+        # instantiate the Preparatory class
+        preps = Preparatory(
+            input_path  =os.path.join(data_dict['output_directory'], 'post_imputation', 'analysis_ready'),
+            input_name  =data_dict['input_prefix'],
+            output_path =data_dict['output_directory'],
+            output_name =data_dict['output_prefix'],
+            dependables =data_dict['dependables'],
         )
 
         # pipeline steps
         prep_steps = {
-            'ld_prune': prep.execute_ld_prunning,
-            'pca'     : prep.execute_pc_decomposition
+            'ld_prune': (preps.execute_ld_prunning, {
+                'maf'     : preparatory_params['maf'], 
+                'geno'    : preparatory_params['geno'],
+                'hwe'     : preparatory_params['hwe'], 
+                'ind_pair': preparatory_params['ind_pair'],
+            }),
+            'pca': (preps.execute_pc_decomposition, {
+                'pca': preparatory_params['pca']
+            }),
         }
 
-        for step in prep_steps.keys():
-            prep_steps[step]()
+        step_description = {
+            'ld_prune': 'Linkage Disequilibrium Prunning',
+            'pca'     : 'Principal Component Analysis'
+        }
+
+        for name, (func, params) in prep_steps.items():
+            print(f"\033[1m{step_description[name]}.\033[0m")
+            func(**params)
 
         print("\033[1mPreprocessing steps completed.\033[0m")
 
-    if steps_dict['gwas_fixed']:
+    if steps_dict['gwas_glm']:
         # instantiate the GWASfixed class
-        gwas_f = GWASfixed(
-            input_path = data_dict['input_directory'],
+        gwas_glm = GWASfixed(
+            input_path = os.path.join(data_dict['output_directory'], 'post_imputation', 'analysis_ready'),
             input_name = data_dict['input_prefix'],
             output_path= data_dict['output_directory'],
             output_name= data_dict['output_prefix'],
-            config_dict= params_dict,
-            preps_path = os.path.join(data_dict['output_directory'], 'preparatory'),
         )
 
         # pipeline steps
-        gwas_f_steps = {
-            'train_model': gwas_f.fixed_model_association_analysis,
-            'top_hits'   : gwas_f.get_top_hits,
-            'annotate'   : gwas_f.annotate_top_hits,
+        gwas_steps = {
+            'train_model': (gwas_glm.fixed_model_association_analysis, {
+                'maf' :gwas_glm_params['maf'], 
+                'mind':gwas_glm_params['mind'], 
+                'hwe' :gwas_glm_params['hwe'], 
+                'ci'  :gwas_glm_params['ci']
+            }),
+            'top_hits'   : (gwas_glm.get_top_hits, {'maf':gwas_glm_params['maf']}),
         }
+        
+        step_description = {
+            'train_model': 'Train the model',
+            'top_hits'   : 'Get top hits'
+        }
+        
+        for name, (func, params) in gwas_steps.items():
+            print(f"\033[1m{step_description[name]}.\033[0m")
+            func(**params)
 
-        for step in gwas_f_steps.keys():
-            gwas_f_steps[step]()
+        print("\033[1mGLM GWAS steps completed.\033[0m")
 
-        print("\033[1mFixed model GWAS steps completed.\033[0m")
-
-    if steps_dict['gwas_random']:
+    if steps_dict['gwas_glmm']:
         # instantiate the GWASrandom class
-        gwas_r = GWASrandom(
-            input_path = data_dict['input_directory'],
+        gwas_glmm = GWASrandom(
+            input_path = os.path.join(data_dict['output_directory'], 'post_imputation', 'analysis_ready'),
             input_name = data_dict['input_prefix'],
             output_path= data_dict['output_directory'],
             output_name= data_dict['output_prefix'],
-            config_dict= params_dict,
-            preps_path = os.path.join(data_dict['output_directory'], 'preparatory'),
         )
 
         # pipeline steps
-        gwas_r_steps = {
-            'preparatory': gwas_r.prepare_aux_files,
-            'grm'        : gwas_r.compute_grm,
-            'random'     : gwas_r.run_gwas_random,
-            'top_hits'   : gwas_r.get_top_hits,
-            'annotate'   : gwas_r.annotate_top_hits,
+        gwas_steps = {
+            'aux_files'  : (gwas_glmm.prepare_aux_files, {}),
+            'compute_grm': (gwas_glmm.compute_grm, {}),
+            'run_gwas'   : (gwas_glmm.run_gwas_random, {'maf' :gwas_glmm_params['maf']}),
+            'top_hits'   : (gwas_glmm.get_top_hits, {'maf' :gwas_glmm_params['maf']})
         }
-        for step in gwas_r_steps.keys():
-            gwas_r_steps[step]()
 
-        print("\033[1mRandom model GWAS steps completed.\033[0m")
+        step_description = {
+            'aux_files'  : 'Prepare auxiliary files',
+            'compute_grm': 'Compute genetic relationship matrix',
+            'run_gwas'   : 'Run GWAS',
+            'top_hits'   : 'Get top hits'
+        }
+
+        for name, (func, params) in gwas_steps.items():
+            print(f"\033[1m{step_description[name]}.\033[0m")
+            func(**params)
+
+        print("\033[1mGLLM GWAS steps completed.\033[0m")
 
     pass
 
